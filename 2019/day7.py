fd6519de2b329a17d19e2dd6d8b71f0a609155e1
Matthew Ramina <mattramina@gmail.com>
2019-12-07 00:46:56 -0500

    day 7 refactored

--- 2019/day7.py
+++ 2019/day7.py
@@ -1,113 +1,40 @@
 from common import *
-from day5 import run
+from intcode import IntcodeVM
+from itertools import permutations
 
 prog = filemap(int, "day7.txt", ',')
-
-from itertools import permutations
 
 m = 0
 for perm in permutations(range(5)):
     out = 0
     for i in perm:
-        out = run(prog, [i, out])[0]
-    # print(out)
+        vm = IntcodeVM(prog)
+        vm.addInput([i, out])
+        vm.run_to_block()
+        out = vm.output[0]
     m = max(out, m)
 
 print(m)
 
-def interpret_op(op):
-    modes = []
-    o = op % 100
-    op //= 100
-    while op > 0:
-        modes.append(op % 10)
-        op //= 10
-    return o, modes
-
-INST_PARAMS = {
-    1: 3,
-    2: 3,
-    3: 1,
-    4: 1,
-    5: 2,
-    6: 2,
-    7: 3,
-    8: 3
-}
-
-def run(data, input_, ip=0):
-    data = list(data)
-    input_ = deque(input_)
-
-    output = []
-    while data[ip] != 99:
-        op, modes = interpret_op(data[ip])
-        modes += [0] * (INST_PARAMS[op] - len(modes))
-        
-        values = []
-        for ix, mode in enumerate(modes):
-            if mode == 0:
-                values.append(data[data[ip+ix+1]])
-            else:
-                values.append(data[ip+ix+1])
-
-        skip_inc = False
-        if op == 1:
-            data[data[ip+3]] = values[0] + values[1]
-        elif op == 2:
-            data[data[ip+3]] = values[0] * values[1]
-        elif op == 3:
-            if len(input_) == 0:
-                # break
-                return output, data, ip
-            data[data[ip+1]] = input_.popleft()
-        elif op == 4:
-            output.append(values[0])
-        elif op == 5:
-            if values[0] != 0:
-                ip = values[1]
-                skip_inc = True
-        elif op == 6:
-            if values[0] == 0:
-                ip = values[1]
-                skip_inc = True
-        elif op == 7:
-            if values[0] < values[1]:
-                data[data[ip+3]] = 1
-            else:
-                data[data[ip+3]] = 0
-        elif op == 8:
-            if values[0] == values[1]:
-                data[data[ip+3]] = 1
-            else:
-                data[data[ip+3]] = 0
-        
-        if not skip_inc:
-            ip += INST_PARAMS[op] + 1
-    return output, data, -1
-
 m = 0
 for perm in permutations(range(5, 10)):
-    programs = [(prog, 0)] * 5
-    perm = list(perm)
-    firstRun = True
+    vms = []
+    for i in perm:
+        vm = IntcodeVM(prog)
+        vm.addInput([i])
+        vms.append(vm)
+
     amp = 0
     out = 0
     while True:
-        if firstRun:
-            inp = [perm[amp], out]
-        else:
-            inp = [out]
-        outL, newProg, newIp = run(programs[amp][0], inp, programs[amp][1])
-        programs[amp] = (newProg, newIp)
-        out = outL[0]
-        if newIp == -1 and amp == 4:
+        vm = vms[amp]
+        vm.addInput([out])
+        halted = vm.run_to_block()
+        out = vm.output[-1]
+        if halted and amp == 4:
             break
         amp += 1
-        if amp == 5:
-            firstRun = False
         amp %= 5
     
-    # print(out)
     m = max(out, m)
 print(m)
---
+++ 2019/intcode.py
@@ -0,0 +1,84 @@
+from collections import deque
+
+INST_PARAMS = {
+    1: 3,
+    2: 3,
+    3: 1,
+    4: 1,
+    5: 2,
+    6: 2,
+    7: 3,
+    8: 3
+}
+
+class IntcodeVM(object):
+    def __init__(self, prog):
+        self.prog = prog
+        self.ip = 0
+        self.input = deque()
+        self.output = []
+
+    def interpret_op(self, op):
+        modes = []
+        o = op % 100
+        op //= 100
+        while op > 0:
+            modes.append(op % 10)
+            op //= 10
+        return o, modes
+
+    def addInput(self, l):
+        self.input.extend(l)
+    
+    def clearOutput(self):
+        self.output = []
+
+    def _setV(self, offset, v):
+        self.prog[self.prog[self.ip + offset]] = v
+
+    def run_to_block(self):
+        while self.prog[self.ip] != 99:
+            op, modes = self.interpret_op(self.prog[self.ip])
+            modes += [0] * (INST_PARAMS[op] - len(modes))
+            
+            values = []
+            for ix, mode in enumerate(modes):
+                if mode == 0:
+                    values.append(self.prog[self.prog[self.ip+ix+1]])
+                else:
+                    values.append(self.prog[self.ip+ix+1])
+
+            skip_inc = False
+            if op == 1:
+                self._setV(3, values[0] + values[1])
+            elif op == 2:
+                self._setV(3, values[0] * values[1])
+            elif op == 3:
+                if len(self.input) == 0:
+                    # blocked
+                    return False
+                self._setV(1, self.input.popleft())
+            elif op == 4:
+                self.output.append(values[0])
+            elif op == 5:
+                if values[0] != 0:
+                    self.ip = values[1]
+                    skip_inc = True
+            elif op == 6:
+                if values[0] == 0:
+                    self.ip = values[1]
+                    skip_inc = True
+            elif op == 7:
+                if values[0] < values[1]:
+                    self._setV(3, 1)
+                else:
+                    self._setV(3, 0)
+            elif op == 8:
+                if values[0] == values[1]:
+                    self._setV(3, 1)
+                else:
+                    self._setV(3, 0)
+            
+            if not skip_inc:
+                self.ip += INST_PARAMS[op] + 1
+        return True
