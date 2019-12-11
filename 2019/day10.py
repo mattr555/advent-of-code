f2cf6573603d170ce5b9cc99763dc3572c38b015
Matthew Ramina <mattramina@gmail.com>
2019-12-10 23:55:32 -0500

    some day 10 cleanups

--- 2019/day10.py
+++ 2019/day10.py
@@ -1,31 +1,7 @@
+from math import gcd, atan2
 from common import *
-from math import gcd, atan2, pi
-from functools import cmp_to_key
 
 data = filemap(lambda x: x, "day10.txt")
-
-# data = '''.#..##.###...#######
-# ##.############..##.
-# .#.######.########.#
-# .###.#######.####.#.
-# #####.##.#.##.###.##
-# ..#####..#.#########
-# ####################
-# #.####....###.#.#.##
-# ##.#################
-# #####.##.###..####..
-# ..######..##.#######
-# ####.##.####...##..#
-# .#####..#.######.###
-# ##...#.##########...
-# #.##########.#######
-# .####.#.###.###.#.##
-# ....##.##.###..#####
-# .#.#.###########.###
-# #.#.#.#####.####.###
-# ###.##.####.##.#..##'''.split('\n')
-
-# print(data.count('#'))
 
 def totalDiv(a, b):
     if b == 0:
@@ -49,33 +25,28 @@ def detects(x, y):
                 else:
                     detected += 1
                     angles[(dx, dy)] = (tx, ty)
-    return detected, x, y, angles
+    return detected, (x, y), angles
 
 
-b = (0, -1, -1, {})
+b = (0, (-1, -1), {})
 for y in range(len(data)):
     for x in range(len(data[0])):
         if data[y][x] == '#':
             b = max(b, detects(x, y))
 print(b[0])
+angles = b[2]
 
 def key(a):
     return -atan2(a[0], a[1])
 
-l = list(sorted(b[3], key=key))
-# poor man's phase shift
+l = list(sorted(angles, key=key))
 
+# poor man's phase shift
 for ix, (i, j) in enumerate(l):
     if i >= 0 and j < 0:
         startIx = ix
         break
 
 l = l[startIx:] + l[:startIx]
-# print(l)
 
-for ix, k in enumerate(l):
-    if ix == 199:
-        # print(b)
-        print(b[3][k][0] * 100 + b[3][k][1])
-
-# print(list(map(lambda i: b[3][i], l)))
+print(angles[l[199]][0] * 100 + angles[l[199]][1])
